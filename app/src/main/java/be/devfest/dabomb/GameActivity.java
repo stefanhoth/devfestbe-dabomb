3ce1290473e1a125e9d63c4ebd612cf98b55240b
Sebastian Mauer <s.mauer@evoreto.com>
2013-11-17 13:11:38 +0100

    Added Bomb Animation

--- app/src/main/java/be/devfest/dabomb/GameActivity.java
+++ app/src/main/java/be/devfest/dabomb/GameActivity.java
@@ -1,16 +1,32 @@
 package be.devfest.dabomb;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import butterknife.InjectView;
+import butterknife.Views;
 
 /**
  * Created by maui on 17.11.13.
  */
 public class GameActivity extends Activity {
 
+    @InjectView(R.id.imageView)
+    ImageView mBomb;
+
+    private AnimationDrawable mBombAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_game);
+        Views.inject(this);
+
+        mBombAnimation = (AnimationDrawable) mBomb.getDrawable();
+        mBombAnimation.start();
     }
 
 }
--- app/src/main/java/be/devfest/dabomb/WelcomeActivity.java
+++ app/src/main/java/be/devfest/dabomb/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
@@ -41,6 +42,9 @@ public class WelcomeActivity extends Activity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        Intent start = new Intent(WelcomeActivity.this, GameActivity.class);
+        startActivity(start);
     }
 
     @Override
