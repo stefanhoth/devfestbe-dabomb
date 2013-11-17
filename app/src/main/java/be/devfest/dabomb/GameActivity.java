a0559ddf946c72d5e60d1352f314e009d3c7ec61
Sebastian Mauer <s.mauer@evoreto.com>
2013-11-17 12:58:14 +0100

    Add GameActivity

---
+++ app/src/main/java/be/devfest/dabomb/GameActivity.java
@@ -0,0 +1,16 @@
+package be.devfest.dabomb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by maui on 17.11.13.
+ */
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
